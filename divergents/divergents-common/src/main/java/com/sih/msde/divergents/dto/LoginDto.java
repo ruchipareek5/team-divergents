098118db4778346fb448b779d91c80e64648d586
prateek-kapoor323 <prateek@smaltandberyl.com>
2018-03-21 23:07:21 +0530

    Author                   : Prateek Files Modified           : LoginService.java LoginDto.java Description              : Added logic for the login process

--- divergents/divergents-common/src/main/java/com/sih/msde/divergents/dto/LoginDto.java
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/dto/LoginDto.java
@@ -1,5 +1,57 @@
 package com.sih.msde.divergents.dto;
 
-public class LoginDto {
+import com.sih.msde.divergents.common.BaseDto;
 
+public class LoginDto extends BaseDto{
+
+	private String userId;
+	//private String password;
+	private String sPOCName;
+	private String userStatus;
+	private String userRole;
+
+	
+	
+	
+	public String getUserStatus() {
+		return userStatus;
+	}
+	public void setUserStatus(String userStatus) {
+		this.userStatus = userStatus;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+//	public String getPassword() {
+//		return password;
+//	}
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+	public String getsPOCName() {
+		return sPOCName;
+	}
+	public void setsPOCName(String sPOCName) {
+		this.sPOCName = sPOCName;
+	}
+	public String getUserRole() {
+		return userRole;
+	}
+	public void setUserRole(String userRole) {
+		this.userRole = userRole;
+	}
+	
+	
+	public LoginDto(String userId, String sPOCName, String userRole, String id) {
+		super();
+		this.userId = userId;
+		//this.password = password;
+		this.sPOCName = sPOCName;
+		this.userRole = userRole;
+		
+	}
+	
 }
--- divergents/divergents-common/src/main/java/com/sih/msde/divergents/service/LoginService.java
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/service/LoginService.java
@@ -1,5 +1,44 @@
 package com.sih.msde.divergents.service;
 
-public class LoginService {
+import java.util.Collection;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sih.msde.divergents.dao.LoginDao;
+import com.sih.msde.divergents.dto.LoginDto;
+import com.sih.msde.divergents.dto.LoginReceiveDataDto;
+
+
+@Service
+public class LoginService {
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginService.class);
+	
+	@Autowired
+	private LoginDao loginDao;
+	private int validUserExistence = -1;
+	
+	private LoginDto loginDto;
+	
+	public LoginDto checkUser(LoginReceiveDataDto loginReceiveDataDto)
+	{
+		validUserExistence = loginDao.userExistsStatus(loginReceiveDataDto.getUserId(),loginReceiveDataDto.getPassword());
+		if(validUserExistence == 1)
+		{
+			LOGGER.debug("User exists in the database");
+			Collection<LoginDto> login= loginDao.getUserDetails(loginReceiveDataDto.getUserId(),loginReceiveDataDto.getPassword());
+			LOGGER.debug("Response receivd from DAO and stored in the collection");
+			LOGGER.debug("Data captured in LoginDto");
+			}
+			
+		else {
+			LOGGER.debug("Wrong Credentials");
+			loginDto = new LoginDto(null,null, null, null);	
+		}
+		
+			LOGGER.debug("Returning Response back to the controller");
+			return loginDto;
+	}
 }

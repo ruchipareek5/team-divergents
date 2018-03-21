bf8973c3498eba0189fd6753b4b16feaafa186e7
ruchipareek5 <ruchipareek5@gmail.com>
2018-03-21 16:37:07 +0530

    Author                      :Ruchi Pareek Description                 :Base files added in folder structure

---
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/common/AbstractTransactionalDao.java
@@ -0,0 +1,20 @@
+package com.sih.msde.divergents.common;
+
+import java.util.Map;
+
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+@Repository
+public class AbstractTransactionalDao extends AbstractQueryDao{
+
+	public int insert(String insertSql, Map<String,Object> parameters,
+		String keyColumnName) {
+		MapSqlParameterSource mapSqlPatMapqlParameterSource = new MapSqlParameterSource(parameters);
+	KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
+	 getJdbcTemplate().update(insertSql, mapSqlPatMapqlParameterSource, generatedKeyHolder,
+			new String[] { keyColumnName });
+	return generatedKeyHolder.getKey().intValue();
+}
+}
---
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/common/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.sih.msde.divergents.common;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+
+
+	@Configuration
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			// @formatter:off
+			http
+					.httpBasic().and()
+					.authorizeRequests()
+					.antMatchers("/index3.html", "/home.html", "/login2.html", "/", "/master.html", "/page1.html","/page2.html","/page3.html", "/page4.html", "/page5.html", "/TrainingPartner.html", "/AssessmentBody.html", "/profileCreationTp.html","/signup").permitAll()
+					.anyRequest().authenticated()
+					.and()
+					.csrf()
+					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+			// @formatter:on
+		}
+	}
+
+

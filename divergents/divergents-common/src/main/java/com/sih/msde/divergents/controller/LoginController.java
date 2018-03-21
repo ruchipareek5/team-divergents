6fb57e42321cad5d53df97ad5b06e358c24d4e2f
prateek-kapoor323 <prateek@smaltandberyl.com>
2018-03-21 23:09:27 +0530

    Author                   : Prateek Files Added              : LoginController.java Description              : Controller for login

---
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.sih.msde.divergents.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sih.msde.divergents.dto.LoginDto;
+import com.sih.msde.divergents.dto.LoginReceiveDataDto;
+import com.sih.msde.divergents.service.LoginService;
+
+@RestController
+public class LoginController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+	
+	@Autowired
+	private LoginService loginService;
+	
+	@RequestMapping(value="/loginUrl",method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
+	public LoginDto getloginDto(LoginReceiveDataDto loginReceiveDataDto)
+	{
+		LOGGER.debug("Sending user Details from the controller to service to validate user with userEmail :" +loginReceiveDataDto.getUserId());
+		return loginService.checkUser(loginReceiveDataDto);
+		
+	}
+}

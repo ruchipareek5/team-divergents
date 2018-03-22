b9116dc2bfdf5bd61c27690a61cdb66bae2dd9ea
poojasharma06 <sharmapooja6797@gmail.com>
2018-03-22 18:04:47 +0530

    Author: Pooja Sharma Description: Modified files and added file Modified File: csrFundingPublicUser.html, csrFundingPublicUser.js,csrFundingPublicUser.html Added File: csrFundingPublicUser.css

--- divergents/divergents-server/src/main/resources/static/js/controller/csrFundingPublicUser.js
+++ divergents/divergents-server/src/main/resources/static/js/controller/csrFundingPublicUser.js
@@ -1,6 +1,8 @@
 var csrFundingPublicUser = angular.module('divergents');
 
-csrFundingPublicUser.controller('csrFundingPublicUser' , function($scope,$http){
+csrFundingPublicUser.controller('csrFundingPublicUser' , function($scope,$http,$window){
+	$scope.login = function(){
+		console.log(angular.toJson($scope.user));
    
 
 });

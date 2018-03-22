19def061a502c6075b2f0bca8a529a9435f3a0ad
poojasharma06 <sharmapooja6797@gmail.com>
2018-03-22 14:03:07 +0530

    Author: Pooja Sharma Description: Modified files and added file Modified File: manageWebinars.html, manageWebinars.js,index.html Added File: manageWebinars.css

--- divergents/divergents-server/src/main/resources/static/js/controller/manageWebinars.js
+++ divergents/divergents-server/src/main/resources/static/js/controller/manageWebinars.js
@@ -2,5 +2,52 @@ var manageWebinars = angular.module('divergents');
 
 manageWebinars.controller('manageWebinars' , function($scope,$http){
    
+	$scope.gridOptions1 = {
+            enableGridMenus : false,
+            enableSorting: false,
+            enableFiltering: false,
+            enableCellEdit : false,
+            enableColumnMenus : false,
+            enableHorizontalScrollbar:0,
+            enableVerticalScrollbar:1,
+            paginationPageSizes: [5, 10, 20, 30],
+            paginationPageSize: 10,
+            useExternalPagination: true,
+            columnDefs : [
+                { name: 'webinarName', displayName: 'Webinar Name', cellClass:'wname'},
+                { name: 'pdfPath', displayName: 'Description Pdf', cellTemplate: '<img src="image/pdf.png" class="pointer" ng-click=grid.appScope.download(row)>', cellClass:'pdf'},
+                { name: 'uploadedOn', displayName: 'Upload Date', cellClass:'date'}
+            ]};
 
+        $http.get('data.json')
+            .then(function(response) {
+                $scope.gridOptions1.data = response.data;
+            });
+
+
+        $scope.gridOptions2 = {
+            enableGridMenus : false,
+            enableSorting: false,
+            enableFiltering: false,
+            enableCellEdit : false,
+            enableColumnMenus : false,
+            enableHorizontalScrollbar:0,
+            enableVerticalScrollbar:1,
+            paginationPageSizes: [5, 10, 20, 30],
+            paginationPageSize: 10,
+            useExternalPagination: true,
+            columnDefs : [
+                { name: 'webinarName', displayName: 'Webinar Name'},
+                { name: 'appName', displayName: 'Applicant Name'},
+                { name: 'contactNo', displayName: 'Contact Number'},
+                { name: 'Email', displayName: 'Email Id'},
+                { name: 'Occupation', displayName: 'Occupation'}
+            ]};
+
+        $http.get('data1.json')
+            .then(function(response) {
+                $scope.gridOptions2.data = response.data;
+            });
 });
+
+

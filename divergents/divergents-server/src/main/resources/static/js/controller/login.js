709cc44494d165cc176df2c9be3dc098e7d5de8b
ruchipareek5 <ruchipareek5@gmail.com>
2018-03-24 03:59:28 +0530

    Author             :Ruchi Pareek Description        :changes to add routing to student

--- divergents/divergents-server/src/main/resources/static/js/controller/login.js
+++ divergents/divergents-server/src/main/resources/static/js/controller/login.js
@@ -6,8 +6,29 @@ login.controller( 'home' , function($scope,$http,$rootScope,$location){
 	$rootScope.templateShow = false;
 	$scope.openStudent = function()
 	{
-		$rootScope.templateShow = true;
+	$rootScope.templateShow = true;
 	var openStudentUrl = "/student";
 	$location.path(openStudentUrl);
-};
+	};
+
+	$scope.openIndustry = function()
+	{
+		$rootScope.templateShowI = true;
+		var openStudentUrl = "/industry";
+		$location.path(openStudentUrl);
+		};
+
+	$scope.openGovt = function(){
+		
+		$rootScope.templateShowGovt = true;
+		var openStudentUrl = "/govt";
+		$location.path(openStudentUrl);
+		};
+	
+	$scope.openTrainingPartner = function(){
+		$rootScope.templateShowTP = true;
+		var openStudentUrl = "/trainingPartner";
+		$location.path(openStudentUrl);
+		};
+	
 });

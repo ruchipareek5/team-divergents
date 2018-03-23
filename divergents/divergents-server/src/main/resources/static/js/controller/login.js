28803df13ef542ba41ae64e8de09d22263b5076f
ruchipareek5 <ruchipareek5@gmail.com>
2018-03-23 09:54:57 +0530

    Author                       :Ruchi Pareek Description                  :Files to add session in front end

--- divergents/divergents-server/src/main/resources/static/js/controller/login.js
+++ divergents/divergents-server/src/main/resources/static/js/controller/login.js
@@ -1,29 +1,2 @@
 var login = angular.module('divergents');
 
-login.controller('loginCtrl' ,['$scope', '$http', function($scope,$http){
-  
-  $scope.login=function(){
-  
-  console.log(angular.toJson($scope.user));
-  var loginControllerURI = "/loginUrl";
-  
-  $http({
-
-			url : loginControllerURI,
-			method : "POST",
-			data : angular.toJson($scope.user),
-			headers : {
-			'Content-type': 'application/json'
-			           }
-
-       }).then(function(response)
-       
-       	{
-        var loginAction=response.data;
-        console.log(loginAction);
-       
-       });
-  }
-
-}
-]);
--- divergents/divergents-server/src/main/resources/static/js/index.js
+++ divergents/divergents-server/src/main/resources/static/js/index.js
@@ -7,7 +7,7 @@ var divergents = angular.module('divergents', ['ngRoute',
                                           'ui.grid.pagination']);
 
 
-divergents.config(['$routeProvider','$httpProvider', function($routeProvider, $httpProvider){
+divergents.config( function($routeProvider, $httpProvider){
 	
 	$routeProvider.when('/nsdc', {
 	    templateUrl : 'nsdcAdmin.html'
@@ -17,7 +17,89 @@ divergents.config(['$routeProvider','$httpProvider', function($routeProvider, $h
 	
 	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 	
-}]);
-divergents.controller('index', function($scope,$http,$rootScope,$route,$location){
+});
+divergents.controller('signupCtrl', function($scope,$http){
 	
 });
+divergents.controller('loginCtrl' , function($rootScope, $http, $location, $route,$scope,$timeout){
+	$scope.credentials = {
+			
+	};
+	 //Signup Function
+	 $scope.signUp = function()
+	 {
+		 
+	 }
+	 //Authenticate Function
+	var authenticate = function(credentials)
+	{
+		var setSession = "/getUserDetails";
+		  var headers = credentials ? {
+	          authorization : "Basic " + btoa(credentials.userId + ":" + credentials.password)
+	        } : {};
+	        $http.get(setSession,{
+	            headers : headers
+	        }).then(function(response){
+	        	if(response.name)
+	        		{
+	        		$rootScope.authenticated = true;
+	        		var loginControllerURI = "/loginUrl";
+	        		  
+	        		  $http({
+
+	        					url : loginControllerURI,
+	        					method : "POST",
+	        					transformResponse: [function (data)  {
+	        	                    //console.log(data);
+
+	        	                    spock = data;
+	        	                    return data;
+	        	                   }],
+	        					//data : angular.toJson($scope.user),
+	        					headers : {
+	        					'Content-type': 'application/json'
+	        					           }
+
+	        		       }).then(function(response)
+	        		       
+	        		       	{
+	        		        var loginAction=response.data;
+	        		        console.log(loginAction);
+	        		       
+	        		       });
+	        		}
+	        	else
+	        		{
+	        		$rootScope.authenticated = false;
+	        		}
+	        }, function(errorResponse){
+	        	$rootScope.authenticated = false;
+	        })
+	}
+	authenticate();
+	$scope.credentials = {
+			
+	};
+
+	//Login function 
+	 $scope.login = function() {
+	        authenticate($scope.credentials, function(){
+	        	if($rootScope.authenticated)
+	        		{
+	        		
+	        		}
+	        	else{
+	        		
+	        	}
+	        });
+	    };
+$scope.logout = function()
+{$http.post("/logout").then(function(response){
+	$rootScope.authenticated = false;
+	$location.path = '/';
+}, function(errorResponse){
+	$rootScope.authenticated = false;
+	$location.path = '/';
+})
+	};
+	});

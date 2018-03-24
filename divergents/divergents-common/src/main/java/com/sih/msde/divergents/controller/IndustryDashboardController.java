4fb6f8360c3865f17da7e126d1100f65d84ce861
prateek-kapoor323 <prateek@smaltandberyl.com>
2018-03-24 21:01:54 +0530

    Author                     : Prateek Files Modified             : IndustryDashboardController.java

--- divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/IndustryDashboardController.java
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/IndustryDashboardController.java
@@ -1,11 +1,16 @@
 package com.sih.msde.divergents.controller;
 
+import java.util.Collection;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sih.msde.divergents.dto.PlacementPercentageOfTopFiveTpDto;
+import com.sih.msde.divergents.dto.TopFiveCorporatesWithMaxCsrContributionsDto;
 import com.sih.msde.divergents.service.IndustryDashboardService;
 
 @RestController
@@ -46,4 +51,29 @@ public class IndustryDashboardController {
 		return industryDashboardService.getTotalTrainingPartners();
 	}
 	
+	@RequestMapping("/getTopFiveCorporates")
+	public Collection <TopFiveCorporatesWithMaxCsrContributionsDto> getTopFiveCorporates()
+	{
+		return industryDashboardService.getTopFiveCorporates();
+	}
+	
+	@RequestMapping("/PlacementPercentageOfTopFiveTp")
+	public Collection<PlacementPercentageOfTopFiveTpDto> getPlacementPercentageOfTopFiveTp(@RequestParam("jobRole") String jobRole)
+	{
+		try {
+			
+			LOGGER.debug("Request Parameter jobRole received in Controller");
+			LOGGER.debug("Sending the request parameter to service");
+			return industryDashboardService.getPlacementPercentageOfTopFiveTp(jobRole);
+		}
+		
+		catch(Exception e) {
+			LOGGER.debug("An error occured while receiving the request parameter jobRole in controller");
+			LOGGER.debug("Error:" +e);
+			LOGGER.debug("Returning Null");
+			return null;
+		}
+		
+	}
 }
+

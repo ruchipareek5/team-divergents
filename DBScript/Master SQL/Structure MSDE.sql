eb3ea66cc409fc4eba8519ffdb3a580b5b0ea3b0
prateek-kapoor323 <prateek@smaltandberyl.com>
2018-03-28 20:32:27 +0530

    Author                   : Prateek Kapoor Files Modified           : Structure MSDE.sql Description              : Added interested TP table

--- DBScript/Master SQL/Structure MSDE.sql
+++ DBScript/Master SQL/Structure MSDE.sql
@@ -834,6 +834,22 @@ CREATE TABLE `user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+
+DROP TABLE IF EXISTS `interestedtp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `interestedtp` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `organisationName` varchar(55) NOT NULL,
+  `location` varchar(100) NOT NULL,
+  `jobRole` varchar(50) NOT NULL,
+  `pocName` varchar(50) NOT NULL,
+  `pocEmail` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;

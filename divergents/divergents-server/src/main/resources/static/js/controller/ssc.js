c143e3490ba0ae5e6a9b469d00bfee804d85230d
ruchipareek5 <ruchipareek5@gmail.com>
2018-03-30 13:04:38 +0530

    Author           : Arjita Files Modified   :ssc.js

--- divergents/divergents-server/src/main/resources/static/js/controller/ssc.js
+++ divergents/divergents-server/src/main/resources/static/js/controller/ssc.js
@@ -2,42 +2,6 @@
 var app = angular.module('divergents');
 app.controller('domain', function($scope) {
     $scope.ssc_list = [
-        {name : "FURNITURE & FITTINGS SKILL COUNCIL", description : "Click Here for Information", image_url: "images/FFSC.png", ssc_url:"http://ffsc.in/"},
-        {name : "TOURISM & HOSPITALITY SECTOR SKILL COUNCIL", description : "Click Here for Information", image_url: "images/THSC.png", ssc_url:"https://www.thsc.in/"},
-        {name : "BEAUTY & WELLNESS SECTOR SKILL COUNCIL", description : "Click Here for Information", image_url: "images/bwd.png", ssc_url:"http://www.bwssc.in/"},
-        {name : "SKILL COUNCIL FOR GREEN JOBS", description : "Click Here for Information", image_url: "images/SCGJ.png", ssc_url:"http://www.sscgj.in"},
-        {name : "APPAREL, MADE-UPS & HOME FURNISHING", description : "Click Here for Information", image_url: "images/AMHSSC.png", ssc_url:"http://sscamh.com/"},
-        
-        {name : "LIFE SCIENCES SECTOR SKILL COUNCIL", description : "Click Here for Information", image_url: "images/LSSSDC.png", ssc_url:"http://lsssdc.in/"},
-        {name : "CONSTRUCTION SKILL DEVELOPMENT COUNCIL OF INDIA", description : "Click Here for Information", image_url: "images/CSDCI.png", ssc_url:"http://www.csdcindia.org/"},
-        {name : "CAPITAL GOODS SKILL COUNCIL", description : "Click Here for Information", image_url: "images/cgsc.png", ssc_url:"http://www.sscgj.in"},
-        
-        {name : "LOGISTICS SECTOR SKILL COUNCIL", description : "Click Here for Information", image_url: "images/LSC.png", ssc_url:"http://www.sscgj.in"},
-        {name : "FOOD INDUSTRY CAPACITY & SKILL INITIATIVE", description : "Click Here for Information", image_url: "images/FICSI.png", ssc_url:"http://www.sscgj.in"},
-    
-        {name : "BFSI SECTOR SKILL COUNCIL OF INDIA", description : "Click Here for Information ", image_url: "images/BFSI.png", ssc_url:"http://www.sscgj.in"},
-        
-        {name : "TELECOM SECTOR SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/TSSC.png", ssc_url:"http://www.sscgj.in"},
-        {name : "ELECTRONICS SECTOR SKILLS COUNCIL", description : "Click Here for Information ",image_url: "images/ESSCI.jpg", ssc_url:"http://www.sscgj.in"}, 
-        {name : "GEMS AND JEWELLERY  SKILL COUNCIL OF INDIA", description : "Click Here for Information ", image_url: "images/gemslogo.jpg", ssc_url:"http://www.sscgj.in"},
-        {name : "HEALTH CARE SECTRO SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/HSSC.png", ssc_url:"http://www.sscgj.in"},
-        {name : "RUBBER SECTOR SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/RSDC.png", ssc_url:"http://www.sscgj.in"},
-        {name : "MEDIA AND ENTERTAINMENT SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/MESC.png", ssc_url:"http://www.sscgj.in"},
-        {name : "IT-ITeS SECTOR SKILL COUNCIL", description : "Click Here for Information ",  image_url: "images/IT-ITeS SSC.png", ssc_url:"http://www.sscgj.in"},
-        						
-        {name : "SECURITY SECTOR SKILL DEVELOPMENT COUNCIL", description : "Click Here for Information ",image_url: "images/security-.png", ssc_url:"http://www.sscgj.in"},
-        {name : "AUTOMOTIVE SKILLS DEVELOPMENT  COUNCIL", description : "Click Here for Information", image_url: "images/ASDC.png", ssc_url:"http://www.sscgj.in"},
-        {name : "AEROSPACE & AVIATION SECTOR SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/AASSC.png", ssc_url:"http://www.sscgj.in"},
-        {name : "POWER SECTOR SKILL COUNCIL", description : "Click Here for Information ",  image_url: "images/PSSC.png", ssc_url:"http://www.sscgj.in"},
-        {name : "SKILL COUNCIL FOR MINING SECTOR", description : "Click Here for Information ", image_url: "images/SCMS.png", ssc_url:"http://www.sscgj.in"},
-        
-        
-        ];
-});
-
-var app = angular.module('divergents');
-app.controller('domain', function($scope) {
-    $scope.ssc_list = [
         {name : "FURNITURE & FITTINGS SKILL COUNCIL", description : "Click Here for Information", image_url: "images/FFSC.png", ssc_url:"http://www.ffsc.in/"},
         {name : "TOURISM & HOSPITALITY SECTOR SKILL COUNCIL", description : "Click Here for Information", image_url:"images/THSC.png", ssc_url:"https://www.thsc.in/"},
         {name : "BEAUTY & WELLNESS SECTOR SKILL COUNCIL", description : "Click Here for Information", image_url: "images/bwd.png", ssc_url:"http://www.bwssc.in/"},
@@ -50,17 +14,17 @@ app.controller('domain', function($scope) {
         
         {name : "LOGISTICS SECTOR SKILL COUNCIL", description : "Click Here for Information", image_url: "images/LSC.png", ssc_url:"http://www.lsc-india.com/"},
         {name : "FOOD INDUSTRY CAPACITY & SKILL INITIATIVE", description : "Click Here for Information", image_url: "images/FICSI.png", ssc_url:"http://www.ficsi.in/"},
-       
+        
         {name : "BFSI SECTOR SKILL COUNCIL OF INDIA", description : "Click Here for Information ", image_url: "images/BFSI.png", ssc_url:"http://www.bfsissc.com/"},
         
         {name : "TELECOM SECTOR SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/TSSC.png", ssc_url:"http://www.tsscindia.com/"},
-        {name : "ELECTRONICS SECTOR SKILLS COUNCIL", description : "Click Here for Information ",image_url: "images/ESSCI.jpg", ssc_url:"http://www.essc-india.org/"}, 
+        {name : "ELECTRONICS SECTOR SKILLS COUNCIL", description : "Click Here for Information ",image_url: "images/ESSCI.jpg", ssc_url:"http://essc-india.org/"}, 
         {name : "GEMS AND JEWELLERY  SKILL COUNCIL OF INDIA", description : "Click Here for Information ", image_url: "images/gemslogo.jpg", ssc_url:"http://www.gjsci.org/"},
         {name : "HEALTH CARE SECTOR SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/HSSC.png", ssc_url:"http://www.healthcare-ssc.in/"},
         {name : "RUBBER SECTOR SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/RSDC.png", ssc_url:"http://www.rsdcindia.in/"},
         {name : "MEDIA AND ENTERTAINMENT SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/MESC.png", ssc_url:"http://www.mescindia.org/"},
         {name : "IT-ITeS SECTOR SKILL COUNCIL", description : "Click Here for Information ",  image_url: "images/IT-ITeS SSC.png", ssc_url:"http://www.sscnasscom.com/"},
-       
+        
         {name : "SECURITY SECTOR SKILL DEVELOPMENT COUNCIL", description : "Click Here for Information ",image_url: "images/security-.png", ssc_url:"http://www.sssdc.in/"},
         {name : "AUTOMOTIVE SKILLS DEVELOPMENT  COUNCIL", description : "Click Here for Information", image_url: "images/ASDC.png", ssc_url:"http://asdc.org.in/"},
         {name : "AEROSPACE & AVIATION SECTOR SKILL COUNCIL", description : "Click Here for Information ", image_url: "images/AASSC.png", ssc_url:"http://www.aassc.in/"},

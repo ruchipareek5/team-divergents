b6d24c601273072024fa947fce95190d51befe2f
prateek-kapoor323 <prateek@smaltandberyl.com>
2018-03-31 02:39:09 +0530

    Author               : Prateek Kapoor Files Modified       : governmentTabsController.js industryTabsController.js File Deleted         : govTabsController.js Description          : Added new controller for gov dashboard

---
+++ divergents/divergents-server/src/main/resources/static/js/controller/governmentTabsController.js
@@ -0,0 +1,241 @@
+var govPartnerTabs = angular.module('divergents');
+
+govPartnerTabs.controller('govPartnerTabs',function($scope,$http)
+		{
+	
+	 $scope.chartType = 'chart3';
+	 $scope.chartTypeList = [{ id:'chart3',name:'Placement of top 5 TP'},{id:'chart4',name:'Maximum CSR contibutions'}];
+
+	 $scope.generateChart = function()
+	  {
+	    if($scope.chartType == 'chart3')
+	      {
+	        //$scope.response = "Ruchi";
+	    	//maxCsrContribution();
+	      }
+	    else if ($scope.chartType == 'chart4')
+	      {
+	    	//$scope.response = "Prateek";
+	         //topFiveTpPlacementPercentage();
+	      }
+	    
+	  }
+	 
+	 $http.get("/getAnnualTarget")
+     .then(function(response) {
+   	  if(response.data == null)
+   		  {
+   		  $scope.annualTarget = 0;
+   		  }
+   	  else
+   		  {
+   		  $scope.annualTarget = response.data;  
+   		  }
+         
+     });
+	 
+	  $http.get("/getTotalCandidatesTrained")
+      .then(function(response) {
+    	  if(response.data)
+    		  {
+    		  $scope.totalCandidatesTrained = 0;
+    		  }
+    	  else
+    		  {
+    		  $scope.totalCandidatesTrained =response.data;
+    		  }
+          
+      });
+	  
+	  $http.get("/getTotalCandidatesCertified")
+      .then(function(response) {
+      	if(response.data == null)
+      		{
+      		$scope.totalCandidatesCertified = 0;
+      		}
+      	else
+      		{
+      		$scope.totalCandidatesCertified = response.data;
+      		}
+          
+      });
+	  
+	  $http.get("/getTotalTrainingCenters")
+      .then(function(response) {
+      	if(response.data == null)
+      		{
+      		$scope.totalTrainingCenters = 0;
+      		}
+      	else
+      		{
+      		$scope.totalTrainingCenters = response.data;
+      		}
+          
+      });
+	  
+	  $http.get("/getTotalCandidatesPlaced")
+      .then(function(response) {
+      	if(response.data == null)
+      		{
+      		$scope.totalCandidatesPlaced = 0;
+      		}
+      	else
+      		{
+      		$scope.totalCandidatesPlaced = response.data;
+      		}
+          
+      });
+	  
+	  
+      //High Charts
+      
+      //Top 5 Corporates with maximum CSR Contribution in last 2 years
+
+      
+    $(document).ready(function() {  
+        
+         var organisationName=[];
+         var csrFunds=[];
+         var csrFundsSum=0;
+         $.getJSON('/getTopFiveCorporates', function(data) {
+            for(var i=0;i<data.length;i++)
+           {
+            organisationName.push(data[i].organisationName);
+            csrFunds.push(data[i].csrFunds);
+               csrFundsSum+=csrFunds[i];
+           }
+           console.log(organisationName);
+          var chart = {
+             type: 'column'
+          };
+          var title = {
+             text: 'Top 5 Corporates with maximum CSR Contribution'   
+          };
+          var subtitle = {
+             text: 'Last 2 years'  
+          };
+          var xAxis = {
+             categories:organisationName,
+             crosshair: true
+          };
+          var yAxis = {
+             min: 0,
+             title: {
+                text: 'CSR Contribution (INR)'         
+             },
+             labels: {
+          formatter:function() {
+              var pcnt = (this.value / csrFundsSum) * 100;
+              return Highcharts.numberFormat(pcnt,0,',') + '%';
+          }
+      }
+          };
+          var tooltip = {
+             headerFormat: '<span style = "font-size:10px">{point.key}</span><table>',
+             pointFormat: '<tr><td style = "color:{series.color};padding:0">{series.name}: </td>' +
+                '<td style = "padding:0"><b>{point.y:.1f} INR</b></td></tr>',
+             footerFormat: '</table>',
+             shared: true,
+             useHTML: true
+          };
+          var plotOptions = {
+             column: {
+                pointPadding: 0.2,
+                borderWidth: 0
+             },
+             
+          };  
+          var credits = {
+             enabled: false
+          };
+          var series= [
+             {
+                name: 'CSR Contribution',
+                data: csrFunds
+             }
+          ];     
+       
+          var json = {};   
+          json.chart = chart; 
+          json.title = title;   
+          json.subtitle = subtitle; 
+          json.tooltip = tooltip;
+          json.xAxis = xAxis;
+          json.yAxis = yAxis;  
+          json.series = series;
+          json.plotOptions = plotOptions;  
+          json.credits = credits;
+          $('#total-cs').highcharts(json);
+
+       });
+        });
+        
+       
+        
+ 
+    
+    $(document).ready(function() {
+  	  
+  	  var processed_json = new Array();
+       
+      $.getJSON("/PlacementPercentageOfTopFiveTp",function(data) {
+         for(var i=0;i<data.length;i++)
+        {
+         processed_json.push([data[i].trainingPartnerName,data[i].totalPlacements]);
+        }
+         
+        var chart = {
+           plotBackgroundColor: null,
+           plotBorderWidth: null,
+           plotShadow: false
+        };
+        var title = {
+           text: 'Top 5 Training Partners with maximum placement percentage'   
+        };      
+        var tooltip = {
+           pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        };
+        var plotOptions = {
+           pie: {
+              allowPointSelect: true,
+              cursor: 'pointer',
+              
+              dataLabels: {
+                 enabled: true,
+                 format: '<b>{point.name}%</b>:{point.percentage:.1f} %',
+                 style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor)||
+                    'black'
+                 }
+              }
+           }
+        };
+        var series = [{
+           type: 'pie',
+           name: 'Total Placements',
+           data: processed_json
+        }];
+        // Radialize the colors
+        Highcharts.getOptions().colors = Highcharts.map(
+           Highcharts.getOptions().colors, function (color) {
+              return {
+                 radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+                 stops: [
+                    [0, color],
+                    [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+                 ]
+              };
+           }
+        );
+	  
+        var json = {};   
+        json.chart = chart; 
+        json.title = title;     
+        json.tooltip = tooltip;  
+        json.series = series;
+        json.plotOptions = plotOptions;
+        $('#placement-percen').highcharts(json);  
+     });
+      });
+	
+		});
--- divergents/divergents-server/src/main/resources/static/js/controller/industryTabsController.js
+++ divergents/divergents-server/src/main/resources/static/js/controller/industryTabsController.js
@@ -4,7 +4,7 @@ industryTabs.controller('industryTabs' , function($scope,$http,$location,$rootSc
   $scope.chartType = 'chart1';
   $scope.chartTypeList = [{ id:'chart1',name:'Placement of top 5 TP'},{id:'chart2',name:'Maximum CSR contibutions'}];
   //write code here
-   $scope.selectedJobRole;
+   //$scope.selectedJobRole;
    
    
   $scope.generateChart = function()
@@ -97,8 +97,7 @@ industryTabs.controller('industryTabs' , function($scope,$http,$location,$rootSc
         //High Charts
         
         //Top 5 Corporates with maximum CSR Contribution in last 2 years
-        
-        
+
         
       $(document).ready(function() {  
           
@@ -180,86 +179,70 @@ industryTabs.controller('industryTabs' , function($scope,$http,$location,$rootSc
           
          
           
-          
-        // Placement Percentage of Top 5 TP in last 2 years
-        
-           $(document).ready(function() {  
-          
-           trainingPartnerName=[];
-           totalPlacements=[];
-           totalPlacemetsSum=0
-           var jobRole=$scope.selectedJobRole; 
-           
-           console.log(jobRole);         
-           $.getJSON("/PlacementPercentageOfTopFiveTp?jobRole="+jobRole,function(data) {
-              for(var i=0;i<data.length;i++)
-             {
-              trainingPartnerName.push(data[i].trainingPartnerName);
-              totalPlacements.push(data[i].totalPlacements);
-                 totalPlacemetsSum += totalPlacements[i];
-             }
-            var chart = {
-               type: 'column'
-            };
-            var title = {
-               text: 'Placement Percentage of Top 5 Training Partners According to Job Role'   
-            };
-            var subtitle = {
-               text: 'Last 2 years'  
-            };
-            var xAxis = {
-               categories: trainingPartnerName,
-               crosshair: true
-            };
-            var yAxis = {
-               min: 0,
-               title: {
-                  text: 'Placement Percentage'         
-               },
-               labels: {
-            formatter:function() {
-                var pcnt = (this.value / totalPlacemetsSum) * 100;
-                return Highcharts.numberFormat(pcnt,0,',') + '%';
-            }
-        }
-            };
-            var tooltip = {
-               headerFormat: '<span style = "font-size:10px">{point.key}</span><table>',
-               pointFormat: '<tr><td style = "color:{series.color};padding:0">{series.name}: </td>' +
-                  '<td style = "padding:0"><b>{point.y:.0f} Placements</b></td></tr>',
-               footerFormat: '</table>',
-               shared: true,
-               useHTML: true
-            };
-            var plotOptions = {
-               column: {
-                  pointPadding: 0.2,
-                  borderWidth: 0
-               },
-               
-            };  
-            var credits = {
-               enabled: false
-            };
-            var series= [
-               {
-                  name: 'Placements',
-                  data: totalPlacements
-               }
-            ];     
+   
+      
+      $(document).ready(function() {
+    	  
+    	  var processed_json = new Array();
          
-            var json = {};   
-            json.chart = chart; 
-            json.title = title;   
-            json.subtitle = subtitle; 
-            json.tooltip = tooltip;
-            json.xAxis = xAxis;
-            json.yAxis = yAxis;  
-            json.series = series;
-            json.plotOptions = plotOptions;  
-            json.credits = credits;
-            $('#placement-percent').highcharts(json);
-  
-         });
-          });			
+        $.getJSON("/PlacementPercentageOfTopFiveTp",function(data) {
+           for(var i=0;i<data.length;i++)
+          {
+           processed_json.push([data[i].trainingPartnerName,data[i].totalPlacements]);
+          }
+           
+          var chart = {
+             plotBackgroundColor: null,
+             plotBorderWidth: null,
+             plotShadow: false
+          };
+          var title = {
+             text: 'Top 5 Training Partners with maximum placement percentage'   
+          };      
+          var tooltip = {
+             pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+          };
+          var plotOptions = {
+             pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                
+                dataLabels: {
+                   enabled: true,
+                   format: '<b>{point.name}%</b>:{point.percentage:.1f} %',
+                   style: {
+                      color: (Highcharts.theme && Highcharts.theme.contrastTextColor)||
+                      'black'
+                   }
+                }
+             }
+          };
+          var series = [{
+             type: 'pie',
+             name: 'Total Placements',
+             data: processed_json
+          }];
+          // Radialize the colors
+          Highcharts.getOptions().colors = Highcharts.map(
+             Highcharts.getOptions().colors, function (color) {
+                return {
+                   radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+                   stops: [
+                      [0, color],
+                      [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+                   ]
+                };
+             }
+          );
+	  
+          var json = {};   
+          json.chart = chart; 
+          json.title = title;     
+          json.tooltip = tooltip;  
+          json.series = series;
+          json.plotOptions = plotOptions;
+          $('#placement-percent').highcharts(json);  
+       });
+        });
 });
+      

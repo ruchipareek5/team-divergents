402ac3c3c29b1c4fbd79e053f50d345328ff36e0
Priyankachaudhary2606 <priyanka@smaltandberyl.com>
2018-03-30 10:00:03 +0530

    Author         : Priyanka Chaudhary Description    : About Us (Header Html)

--- divergents/divergents-server/src/main/resources/static/js/controller/about-us.js
+++ divergents/divergents-server/src/main/resources/static/js/controller/about-us.js
@@ -2,5 +2,28 @@ var aboutUs = angular.module('divergents');
 
 aboutUs.controller('aboutUs', function($scope, $http)
 		{
-	
-		});
+    
+    $scope.sectors =[
+  {
+    'id': '1',
+     'name': "Aquitaine",
+     'categories': [{
+        'id': '22',
+        'dep': "Dordogne"
+    }, {
+        'id': '31',
+        'dep': "Gironde"
+    }]
+  },
+  {
+    'id': '2',
+     'name': "Auvergne",
+     'categories':  [{
+        'id': '3',
+        'dep': "Allier"
+    }, {
+        'id': '15',
+        'dep': "Cantal"
+    }]
+  }];
+});

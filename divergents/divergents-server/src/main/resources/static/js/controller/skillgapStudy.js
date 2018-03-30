b1faf861ef77487df821779cbbce83662022c4cc
poojasharma06 <sharmapooja6797@gmail.com>
2018-03-31 03:22:28 +0530

    Author: Pooja Sharma Description: skillgapstudy js file modified File: skillgapStudy.js

--- divergents/divergents-server/src/main/resources/static/js/controller/skillgapStudy.js
+++ divergents/divergents-server/src/main/resources/static/js/controller/skillgapStudy.js
@@ -3,40 +3,7 @@ var gapStudy = angular.module('divergents');
 gapStudy.controller('gapStudy' , function($scope,$http){
 	$scope.showDetail= false;
 	$scope.tab = "Statewise";
-	$scope.stateList=[
-	                  {"name" :"Andhra Pradesh"},
-	                  {"name" :"Arunachal Pradesh"},
-	                  {"name" :"Assam"},
-	                  {"name" :"Bihar"},
-	                  {"name" :"Chhattisgarh"},
-	                  {"name" :"Delhi"},
-	                  {"name" :"Goa"},
-	                  {"name" :"Gujarat"},
-	                  {"name" :"Haryana"},
-	                  {"name" :"Himachal Pradesh"},
-	                  {"name" :"Jammu & Kashmir"},
-	                  {"name" :"Jharkhand"},
-	                  {"name" :"Karnataka"},
-	                  {"name" :"Kerala"},
-	                  {"name" :"Madhya Pradesh"},
-	                  {"name" :"Maharashtra"},
-	                  {"name" :"Manipur"},
-	                  {"name" :"Meghalaya"},
-	                  {"name" :"Mizoram"},
-	                  {"name" :"Nagaland"},
-	                  {"name" :"Odisha"},
-	                  {"name" :"Punjab"},
-	                  {"name" :"Rajasthan"},
-	                  {"name" :"Sikkim"},
-	                  {"name" :"Tamil Nadu"},
-	                  {"name" :"Telangana"},
-	                  {"name" :"Tripura"},
-	                  {"name" :"Uttarakhand"},
-	                  {"name" :"Uttar Pradesh"},
-	                  {"name" :"West Bengal"}
-	                  ];
-	
-	$scope.gridOptionstate = {
+    $scope.gridOptionstate = {
 	         enableGridMenus : false,  
 	         enableSorting: false, 
 	         enableFiltering: false,
@@ -61,8 +28,7 @@ gapStudy.controller('gapStudy' , function($scope,$http){
    .error(function (error) {
       console.log("Error"+ error);
    });
-	                  
-	$scope.reports=[" Education & Skill Development Services"," Furniture & Furnishings industry"," Media & Entertainment"," Organized Retail sector"," Pharmaceuticals"," Private Security Services"," Telcommunications","Agriculture","Haryana","Himachal Pradesh","Jammu & Kashmir","Jharkhand","Karnataka","Kerala","Madhya Pradesh","Maharashtra","Manipur","Meghalaya","Mizoram","Nagaland","Odisha","Punjab","Rajasthan","Sikkim","Tamil Nadu","Telangana","Tripura","Uttarakhand","Uttar Pradesh","West Bengal"];
+	              
 	$scope.showYourInterest=function(){
 		if($scope.showInterest== false){
 		$scope.showInterest= true;}

de43ad202a1fd43894378915ad57b2adfec5640b
Priyankachaudhary2606 <priyanka@smaltandberyl.com>
2018-03-31 13:24:03 +0530

    Author    : Priyanka Description : FeedbackController updated

--- divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/FeedbackController.java
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/controller/FeedbackController.java
@@ -5,6 +5,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sih.msde.divergents.dto.FeedbackDto;
@@ -19,8 +20,17 @@ public class FeedbackController {
 	private FeedbackService feedbackService;
 	
 	
+	
+	
+	
+	
 	@RequestMapping(value="/submitFeedback",method=RequestMethod.POST,consumes=MediaType.ALL_VALUE)
-	public Integer submitthefeedback(@RequestBody FeedbackDto feedbackDto){
+	public Integer submitthefeedback(@RequestParam("name") String name, @RequestParam("aadharnumber") String aadharnumber, @RequestParam("email") String email, @RequestParam("suggestion") String suggestion){
+		FeedbackDto feedbackDto = new FeedbackDto(name,aadharnumber,email,suggestion);
+		feedbackDto.setName(name);
+		feedbackDto.setAadharnumber(aadharnumber);
+		feedbackDto.setEmail(email);
+		feedbackDto.setSuggestion(suggestion);
 		return feedbackService.submitFeedback(feedbackDto);
 	}
 	

e3d50358a32b83fd7f79c8d9957264fe7c1b76be
poojasharma06 <sharmapooja6797@gmail.com>
2018-04-04 18:59:36 +0530

    Author: Pooja Sharma Description: search functionality and active tenders modified File: ShowTendersDto.java

--- divergents/divergents-common/src/main/java/com/sih/msde/divergents/dto/ShowTendersDto.java
+++ divergents/divergents-common/src/main/java/com/sih/msde/divergents/dto/ShowTendersDto.java
@@ -7,6 +7,9 @@ public class ShowTendersDto {
 	private String tenderTitle;
 	private String tenderCategory;
 	private String tenderDepartment;
+	private String tenderDocument;
+	private String bidStartDate;
+	private String bidSubmissionDate;
 	public String getTenderNumber() {
 		return tenderNumber;
 	}
@@ -31,12 +34,33 @@ public class ShowTendersDto {
 	public void setTenderDepartment(String tenderDepartment) {
 		this.tenderDepartment = tenderDepartment;
 	}
-	public ShowTendersDto(String tenderNumber, String tenderTitle, String tenderCategory, String tenderDepartment) {
+	public String getTenderDocument() {
+		return tenderDocument;
+	}
+	public void setTenderDocument(String tenderDocument) {
+		this.tenderDocument = tenderDocument;
+	}
+	public String getBidStartDate() {
+		return bidStartDate;
+	}
+	public void setBidStartDate(String bidStartDate) {
+		this.bidStartDate = bidStartDate;
+	}
+	public String getBidSubmissionDate() {
+		return bidSubmissionDate;
+	}
+	public void setBidSubmissionDate(String bidSubmissionDate) {
+		this.bidSubmissionDate= bidSubmissionDate;
+	}
+	public ShowTendersDto(String tenderNumber, String tenderTitle, String tenderCategory, String tenderDepartment, String tenderDocument, String bidStartDate,String bidSubmissionDate) {
 		super();
 		this.tenderNumber = tenderNumber;
 		this.tenderTitle = tenderTitle;
 		this.tenderCategory = tenderCategory;
 		this.tenderDepartment = tenderDepartment;
+		this.tenderDocument = tenderDocument;
+		this.bidStartDate = bidStartDate;
+		this.bidSubmissionDate= bidSubmissionDate;
 	}
 	public ShowTendersDto() {
 		super();
